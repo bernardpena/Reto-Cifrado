d47be781c62a26d4d586ae9fa39d8b1acc11b913
bernardo Peña <bernardpena@gmail.com>
2023-01-24 06:12:11 -0300

    inico de captura de texto

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 
 var botonEncriptar =document.querySelector(".btn-Encriptar");
 var botonDesencriptar =document.querySelector(".btn-Desencriptar");
+var muneco = document.querySelector(".contenedorImagen")
+var h3 = document.querySelector(".contenedorSubtitulo")
+var p = document.querySelector(".contenedorParrafos")
 
-botonEncriptar.onclick = encriptar();
+botonEncriptar.onclick = encriptar;
+
+function encriptar(){
+    ocultarOtros();
+    
+
+}
+
+function recuperarTexto(){
+    var texto =document.querySelector(".textarea");
+    return texto.value;
+
+}
+
+function ocultarOtros(){
+    muneco.classList.add("ocultar");
+    h3.classList.add("ocultar");
+    p.classList.add("ocultar");
+
+}
